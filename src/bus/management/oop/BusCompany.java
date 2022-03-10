958e8e6fe3899874d1f87986f465f103fe461477
Emre Can Satık <emrecanstk@gmail.com>
2022-03-11 01:39:20 +0300

    add attributes on company

--- src/bus/management/oop/BusCompany.java
+++ src/bus/management/oop/BusCompany.java
@@ -1,7 +1,13 @@
 package bus.management.oop;
 
 public class BusCompany {
-    
+
+    static String name = "Kamil Koc";
+    static String phoneNumber = "+90 534 252 5094";
+    int earnedMoney;
+    int spentMoney;
+    int totalMoney;
+
     private static BusCompany company = new BusCompany();
 
     private BusCompany() {

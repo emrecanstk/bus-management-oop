0165532b451ea8052c643323fbf73e829c43990f
Emre Can Satık <emrecanstk@gmail.com>
2022-03-11 16:12:42 +0300

    test 2

--- src/bus/management/oop/Main.java
+++ src/bus/management/oop/Main.java
@@ -14,11 +14,11 @@ public class Main {
         Bus bus6 = new Bus(6);
         Bus bus7 = new Bus(7);
 
-        Journey mkp_corlu = new Journey(1,"Mustafakemalpaşa - Çorlu",50,340);
-        Journey istanbul_izmir = new Journey(2,"İstanbul - İzmir",60,360);
-        Journey ankara_eskisehir = new Journey(3,"Ankara - Eskişehir",20,120);
-        Journey bursa_manisa = new Journey(4,"Bursa - Manisa",30,160);
-        Journey tokat_adana = new Journey(5,"Tokat - Adana",70,440);
+        Journey mkp_corlu = new Journey(1,"Mustafakemalpaşa - Çorlu",90,340);
+        Journey istanbul_izmir = new Journey(2,"İstanbul - İzmir",100,360);
+        Journey ankara_eskisehir = new Journey(3,"Ankara - Eskişehir",60,120);
+        Journey bursa_manisa = new Journey(4,"Bursa - Manisa",70,160);
+        Journey tokat_adana = new Journey(5,"Tokat - Adana",110,440);
 
         Driver tony = new Driver(1,"Tony Stark",290);
         Driver steve = new Driver(2,"Steve Rogers",180);
@@ -30,7 +30,7 @@ public class Main {
 
         Client emre = new Client(1,"Emre Can Satık");Client gulnur = new Client(2,"Gülnur Vırdınlı");
         Client melike = new Client(3,"Melike Satık");Client ercan = new Client(4,"Ercan Satık");
-        Client aycan = new Client(5,"Aycan Satık");Client mert = new Client(13,"Mert Manisa");
+        Client aycan = new Client(5,"Aycan Satık");Client mert = new Client(13,"Mert Arman");
         Client murat = new Client(6,"Murat Pangal");Client ali = new Client(14,"Ali Paslı");
         Client serdar = new Client(7,"Serdar Salar");Client ozkan = new Client(15,"Özkan Züngör");
         Client emir = new Client(8,"Emir Özbatmaz");Client enes = new Client(16,"Enes Bayız");
@@ -47,6 +47,22 @@ public class Main {
         kale.BuyBus(bus6);
         kale.BuyBus(bus7);
 
+        bus1.StartJourney(mkp_corlu,tony);
+        bus5.StartJourney(ankara_eskisehir,tony);
+        bus6.StartJourney(ankara_eskisehir,clint);
+
+        emre.BuyTicket(bus1,mkp_corlu);
+        gulnur.BuyTicket(bus1,mkp_corlu);
+        ozkan.BuyTicket(bus1,mkp_corlu);
+        mert.BuyTicket(bus1,mkp_corlu);
+        melike.BuyTicket(bus1,mkp_corlu);
+        ercan.BuyTicket(bus1,mkp_corlu);
+        aycan.BuyTicket(bus1,mkp_corlu);
+        ali.BuyTicket(bus1,mkp_corlu);
+        cihan.BuyTicket(bus1,mkp_corlu);
+        eray.BuyTicket(bus1,mkp_corlu);
+
+        System.out.println(bus1.getTotalMoney());
 
     }
 
